ad1415746476a8897222ea1b09d32af5f4352ecd
Julio Denis <julio_denis@live.com>
2020-09-10 00:15:48 -0500

    redux and store implemented

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { Provider } from "react-redux";
+import reducer from "./reducers/index";
+import { createStore } from "redux";
+
+// Global statse
+const initialState = {};
+const store = createStore(reducer, initialState);
 
 // Render del componente principal al HTML
-ReactDOM.render(<App />, document.getElementById("App"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("App")
+);
