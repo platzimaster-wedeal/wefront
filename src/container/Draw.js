758ad0cfec79a21f9b59287a32e377d1b1a60f90
Julio Denis <julio_denis@live.com>
2020-09-22 02:20:41 -0500

    [REFACTOR] SmallButtonCA component added

--- src/container/Draw.js
+++ src/container/Draw.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { MdCameraAlt, MdAdd, MdModeEdit } from "react-icons/md";
-import SmallButtonSA from "../components/Buttons/SmallButtonSA";
+import { MdCameraAlt, MdClose } from "react-icons/md";
+import SmallButtonCA from "../components/Buttons/SmallButtonCA";
 import "../assets/styles/components/Draw/Draw.scss";
 
 const Draw = () => {
  return (
   <div className="button__container">
-   <SmallButtonSA icon={MdCameraAlt} width="100px" height="100px" />
-   <SmallButtonSA icon={MdAdd} width="100px" height="100px" />
-   <SmallButtonSA icon={MdModeEdit} width="100px" height="100px" />
+   <SmallButtonCA
+    icon={MdCameraAlt}
+    width="100px"
+    height="100px"
+    closeBtn={false}
+   />
+   <SmallButtonCA icon={MdClose} width="100px" height="100px" closeBtn />
   </div>
  );
 };
