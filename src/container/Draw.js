d39754349cba9e097632beed0fc07455f0ac43e1
= <=>
2020-09-23 11:26:41 -0500

    ➕ [ADD] requirements component added

--- src/container/Draw.js
+++ src/container/Draw.js
@@ -1,9 +1,10 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { MdCameraAlt, MdClose, MdFilterList, MdModeEdit } from "react-icons/md";
 import SmallButtonCA from "../components/Buttons/SmallButtonCA";
 import SmallButtonSA from "../components/Buttons/SmallButtonSA";
 import ButtonAction from "../components/Buttons/ButtonAction";
 import Label from "../components/Labels/Labels";
+import Requirements from "../components/Requirements/Requirements";
 import "../assets/styles/components/Draw/Draw.scss";
 
 const Draw = () => {
@@ -27,6 +28,7 @@ const Draw = () => {
    </div>
    <div className="andres__components__test container__test">
     <Label name="Enginner" active />
+    <Requirements title="Knowledge about hardaware of PCs" />
    </div>
   </>
  );
