5a93166b8b65e01bdbe0933ea944ae9b380bb4e3
Julio Denis <julio_denis@live.com>
2020-09-22 21:46:55 -0500

    [🛠 FIX] Draw component fixed

--- src/container/Draw.js
+++ src/container/Draw.js
@@ -10,7 +10,7 @@ import "../assets/styles/components/Draw/Draw.scss";
 const Draw = () => {
  return (
   <>
-   <div className="button__container">
+   <div className="julio__components__test container__test">
     <SmallButtonCA
      icon={MdCameraAlt}
      width="100px"
@@ -27,23 +27,6 @@ const Draw = () => {
     />
     <Button active>Make Deal!</Button>
     <Button>Make Deal!</Button>
-   </div>
-
-   <div className="julio__components__test container__test">
-    <SmallButtonCA
-     icon={MdCameraAlt}
-     width="100px"
-     height="100px"
-     closeBtn={false}
-    />
-    <SmallButtonCA icon={MdClose} width="100px" height="100px" closeBtn />
-    <ButtonAction icon={MdFilterList} width="100px" height="100px" />
-    <SmallButtonSA
-     icon={MdModeEdit}
-     width="100px"
-     height="100px"
-     fontSize="60px"
-    />
    </div>
    <div className="andres__components__test container__test">
     <Label name="Enginner" active />
