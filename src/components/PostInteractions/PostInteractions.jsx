3ec04a5169293e97839a9fa2e26dd6461d21aab9
Juanes <jedp082@gmail.com>
2020-09-23 21:21:54 -0500

    [REFACTOR] - Modify the name and a little the styles of InteractionComponet

--- src/components/PostInteractions/PostInteractions.jsx
+++ src/components/PostInteractions/PostInteractions.jsx
@@ -5,7 +5,7 @@ import { MdShare } from "react-icons/md";
 import { MdInsertComment } from "react-icons/md";
 import "../../assets/styles/components/InteractionsPost/InteractionsPost.scss";
 
-const InteractionsPost = ({ favs, shares, comments }) => {
+const PostInteractions = ({ favs, shares, comments }) => {
  const [activeFavs, setActiveFavs] = useState(false);
  const [activeShare, setActiveShare] = useState(false);
  const [activeComments, setActiveComments] = useState(false);
@@ -28,28 +28,33 @@ const InteractionsPost = ({ favs, shares, comments }) => {
     onClick={() => handelFavsClick()}
     type="button"
     className="interactionsPost__favs">
-    {activeFavs ? <MdFavorite className='interactionsPost__icon--active' /> : <MdFavoriteBorder className='interactionsPost__icon--inactive' />}
+    {activeFavs ? (
+     <MdFavorite className="interactionsPost__icon--active" />
+    ) : (
+     <MdFavoriteBorder className="interactionsPost__icon--inactive" />
+    )}
     {favs} Favs
    </button>
    <button
-   onClick={() => handelShareClick()}
-   type="button"
-   className="interactionsPost__shares "
-   >
-     {
-       activeShare ? <MdShare className='interactionsPost__icon--active' /> : <MdShare className='interactionsPost__icon--inactive' />
-     }
+    onClick={() => handelShareClick()}
+    type="button"
+    className="interactionsPost__shares ">
+    {activeShare ? (
+     <MdShare className="interactionsPost__icon--active" />
+    ) : (
+     <MdShare className="interactionsPost__icon--inactive" />
+    )}
     {shares} Shares
    </button>
    <button
-   onClick={() => handelCommentsClick()}
-   type="button"
-   className="interactionsPost__comments"
-   >
-     {
-       activeComments ? <MdInsertComment className='interactionsPost__icon--active' /> : <MdInsertComment className='interactionsPost__icon--inactive' />
-     }
-    
+    onClick={() => handelCommentsClick()}
+    type="button"
+    className="interactionsPost__comments">
+    {activeComments ? (
+     <MdInsertComment className="interactionsPost__icon--active" />
+    ) : (
+     <MdInsertComment className="interactionsPost__icon--inactive" />
+    )}
     {comments} Comments
    </button>
    <button></button>
@@ -57,4 +62,4 @@ const InteractionsPost = ({ favs, shares, comments }) => {
  );
 };
 
-export default InteractionsPost;
+export default PostInteractions;
