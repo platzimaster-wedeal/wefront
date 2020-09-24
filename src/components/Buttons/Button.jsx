803225761273089f9a259550309e094b48f62f41
Julio Denis <julio_denis@live.com>
2020-09-24 17:47:46 -0500

    [➕ADD] PersonCard componend added [♻️ REFACTOR] All buttons with responsive [🛠FIX] Code has been formatted

--- src/components/Buttons/Button.jsx
+++ src/components/Buttons/Button.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../../assets/styles/components/Buttons/Button.scss";
 
-const Button = ({ children, active = false }) => {
+const Button = ({ children, active = false, className }) => {
  return active ? (
-  <button className="button button__primary">{children}</button>
+  <button className={`button button__primary ${className}`}>{children}</button>
  ) : (
-  <button className="button button__secondary">{children}</button>
+  <button className={`button button__secondary ${className}`}>
+   {children}
+  </button>
  );
 };
 
--- src/components/Buttons/SmallButtonCA.jsx
+++ src/components/Buttons/SmallButtonCA.jsx
@@ -1,29 +1,21 @@
 import React from "react";
 import "../../assets/styles/components/Buttons/SmallButtonCA.scss";
 
-const buttonSize = (width, height, fontSize, closeBtn) => {
+const buttonSize = (closeBtn) => {
  if (closeBtn) {
   return {
    backgroundColor: " hsl(152,59%,94%)",
    color: " hsla(240,27%,12%,.6)",
-   width: width || "70px",
-   height: height || "70px",
-   fontSize: fontSize || "70px",
   };
  }
- return {
-  width: width || "70px",
-  height: height || "70px",
-  fontSize: fontSize || "70px",
- };
 };
 
-const SmallButtonCA = ({ icon, width, height, fontSize, closeBtn }) => {
+const SmallButtonCA = ({ icon, closeBtn, className }) => {
  const MdIcon = icon;
  return (
   <button
-   className="button-actionCA"
-   style={buttonSize(width, height, fontSize, closeBtn)}>
+   className={`button-actionCA ${className}`}
+   style={buttonSize(closeBtn)}>
    <MdIcon />
   </button>
  );
