3a4c17083c0d32845d4ff48e8f9a9185e9a93a01
Julio Denis <julio_denis@live.com>
2020-09-29 16:55:50 -0500

    [➕ADD] SelectDate component added

---
+++ src/container/Draw.js
@@ -0,0 +1,9 @@
+import React from "react";
+import SelectDate from "../components/DatePicker/SelectDate";
+import "../assets/styles/components/Draw/Draw.scss";
+
+const Draw = () => {
+ return <SelectDate />;
+};
+
+export default Draw;
