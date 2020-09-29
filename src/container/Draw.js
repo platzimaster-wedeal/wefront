2ef65f95c902a032979d62b5df77da3e4e0e5431
Juanes <jedp082@gmail.com>
2020-09-28 19:50:34 -0500

    [ADD][REFACTOR] - Created HeaderSearchFilters with tests and responsive, also fix some test of PostProblemDetails

--- src/container/Draw.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import HeaderSearchFilters from "../components/HeaderSearchFilters/HeaderSearchFilters";
-
-const Draw = () => <HeaderSearchFilters />;
-
-export default Draw;
